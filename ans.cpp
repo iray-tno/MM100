3a9e83fa9ff99b6811d091cc298067d192a62825
iray-tno <iray.tno@gmail.com>
2018-04-20 01:34:20 +0900

    Add an example

--- ans.cpp
+++ ans.cpp
@@ -47,11 +47,49 @@ public:
     double getElapsed() { return getSec() - start_time; }
 };
 
+/**
+ * SameColorPairs
+ */
+class SameColorPairs {
+public:
+    vector<string> removePairs(vector<string> board) {
+        vector<string> ret;
+        int H = board.size(), W = board[0].size();
+        // find the first pair of horizontally adjacent tiles and remove them
+        for (int i = 0; i < H; ++i)
+        for (int j = 1; j < W; ++j)
+            if (board[i][j] == board[i][j-1]) {
+                ret.push_back(to_string(i) + " " + to_string(j) + " " + to_string(i) + " " + to_string(j-1));
+                return ret;
+            }
+        return ret;
+    }
+};
+
 #ifdef LOCAL_DEBUG
 // -------8<------- end of solution submitted to the website -------8<-------
+
+template<class T> void getVector(vector<T>& v) {
+    for (int i = 0; i < v.size(); ++i)
+        cin >> v[i];
+}
+
 int main(int, char **) {
     cin.tie(0); ios::sync_with_stdio(false);
 
+    SameColorPairs scp;
+    int H;
+    cin >> H;
+    vector<string> board(H);
+    getVector(board);
+
+    vector<string> ret = scp.removePairs(board);
+    cout << ret.size() << endl;
+    for (int i = 0; i < (int)ret.size(); ++i)
+        cout << ret[i] << endl;
+    cout.flush();
+
     return 0;
 }
+
 #endif
