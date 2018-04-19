d2cc5998325324d3b01e39ba7c4e1d830d9122a5
iray-tno <iray.tno@gmail.com>
2018-04-20 01:35:59 +0900

    Improve .bat files

--- build.bat
+++ build.bat
@@ -1,3 +1,2 @@
-clang++ -target x86_64-pc-windows-gnu ./ans.cpp -O3
-
-powershell -C (Measure-Command {"((Get-Content ./input.txt | ./a.exe > hoge.txt))"}).TotalMilliseconds
+clang++ -target x86_64-pc-windows-gnu ./ans.cpp -O3 -D LOCAL_DEBUG
+java -jar tester.jar -exec a.exe -novis
---
+++ run.bat
@@ -0,0 +1,19 @@
+@echo off
+powershell -C ((Measure-Command {"((java -jar tester.jar -exec a.exe -novis > temp/score.txt))"}).TotalMilliseconds) > temp/time.txt
+
+mkdir temp 2> nul
+
+type .\temp\score.txt > .\temp\score2.txt
+for /f "tokens=1,2,3" %%a in (.\temp\score2.txt) do (
+    set SCORE=%%c
+)
+
+for /f %%a in (.\temp\time.txt) do (
+    set TIME=%%a
+)
+
+echo score: %SCORE%, time: %TIME%
+echo %SCORE%, %TIME%>> results.txt
+
+del temp /Q
+@echo on
