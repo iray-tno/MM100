a96f1d8a8b7895291454b7c4bf75e170fa93c3f0
iray-tno <iray.tno@gmail.com>
2018-04-21 16:04:32 +0900

    Add simple greedy

--- ans.cpp
+++ ans.cpp
@@ -1,13 +1,20 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <algorithm>
+#include <utility>
 #include <cmath>
+#include <cassert>
 
 using namespace std;
 
 #ifdef LOCAL_DEBUG
-// 使うクラスの定義
+#undef _MSC_VER
 #endif
+
+template<typename T> inline bool isWithin(T begin, T value, T end) { return begin <= value && value < end; }
+constexpr int dx[] = { 0, 1, 0, -1 };
+constexpr int dy[] = { 1, 0, -1, 0 };
 
 static unsigned int x = 123456789, y = 362436069, z = 521288629, w = 88675123;
 inline unsigned int randxor() {
@@ -47,6 +54,83 @@ public:
     double getElapsed() { return getSec() - start_time; }
 };
 
+typedef char Color;
+
+typedef vector<Color> BoardRow;
+typedef vector<BoardRow> Board;
+
+class Field {
+    public:
+    Board board;
+    int h, w;
+
+    Field();
+    Field(int h_, int w_, vector<string> board_): h(h_), w(w_) {
+        board = Board(h, BoardRow(w, -1));
+
+        for (int i = 0; i < h; ++i) {
+            for (int j = 0; j < w; ++j) {
+                board[i][j] = board_[i][j] - '0';
+            }
+        }
+    }
+
+    void dump() {
+        for (int i = 0; i < h; ++i) {
+            for (int j = 0; j < w; ++j) {
+                cerr << (int) board[i][j];
+            }
+            cerr << endl;
+        }
+        return;
+    }
+
+    vector<string> hoge() {
+        vector<string> seq;
+        for (int x = 0; x < h; ++x) {
+            for (int y = 0; y < w; ++y) {
+                for (int i = 0; i < 4; ++i) {
+                    if (board[x][y] < 0) continue;
+
+                    int nx = x + dx[i];
+                    int ny = y + dy[i];
+
+                    if (!isWithin(0, nx, h)) continue;
+                    if (!isWithin(0, ny, w)) continue;
+                    if (board[x][y] != board[nx][ny]) continue;
+
+                    board[x][y] = -1;
+                    board[nx][ny] = -1;
+                    seq.push_back(to_string(x) + " " + to_string(y) + " " + to_string(nx) + " " + to_string(ny));
+                }
+            }
+        }
+        return seq;
+    }
+
+    bool check(int ax, int ay, int bx, int by) {
+        assert(isWithin(0, ax, h)); assert(isWithin(0, ay, w));
+        assert(isWithin(0, bx, h)); assert(isWithin(0, by, w));
+
+        if (bx < ax) swap(ax, bx);
+        if (by < ay) swap(ay, by);
+
+        if(board[ax][ay] != board[bx][by]) return false;
+        if(ax == bx && ay == by) return false;
+        if(board[ax][ay] < 0) return false;
+
+        Color color = board[ax][ay];
+
+        for (int i = ax; i <= bx; ++i) {
+            for (int j = ay; j <= by; ++j) {
+                if (board[i][j] != color) return false;
+            }
+        }
+
+        return true;
+    }
+};
+
 /**
  * SameColorPairs
  */
@@ -55,14 +139,11 @@ public:
     vector<string> removePairs(vector<string> board) {
         vector<string> ret;
         int H = board.size(), W = board[0].size();
-        // find the first pair of horizontally adjacent tiles and remove them
-        for (int i = 0; i < H; ++i)
-        for (int j = 1; j < W; ++j)
-            if (board[i][j] == board[i][j-1]) {
-                ret.push_back(to_string(i) + " " + to_string(j) + " " + to_string(i) + " " + to_string(j-1));
-                return ret;
-            }
-        return ret;
+
+        Field field(H, W, board);
+        // field.dump();
+
+        return field.hoge();
     }
 };
 
